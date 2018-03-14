55696a9daf211d8a3aa32a53130fba8051cdd4cf
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-14 17:44:00 +0000

    fixed display method to hand spaces

--- hidden_word.rb
+++ hidden_word.rb
@@ -2,21 +2,59 @@
 
 class Hidden_Word
 
-attr_reader :word, :display
+  attr_reader :hidden_word
 
   def initialize(input_word)
 
-    @word = input_word
-    @display = string_to_hidden
+    @hidden_word = input_word
+
+
 
   end
 
-  def string_to_hidden()
-    return "*" * @word.length
+  # def display(guesses = [])
+  #   displayed_word = ""
+  #   for char in @hidden_word.split('')
+  #     if guesses.include?(char) || char == " "
+  #       displayed_word << char
+  #     else
+  #       displayed_word << "*"
+  #     end
+  #   end
+  #   return displayed_word
+  # end
+  #
+  # def display
+  #   word = @hidden_word
+  #   answer = @hidden_word.gsub(/[#{word}]/, "*")
+  #   return answer
+  # end
+
+  def display
+    word_length = @hidden_word.length
+    for i in 0...word_length
+      unless @hidden_word[i] == " " then @hidden_word[i] = "*"
+      end
+    end
+    return @hidden_word
   end
+  #
+  # def display_guess(guessed_letter)
+  #   word_length = @hidden_word.length
+  #   for i in 0...word_length
+  #     unless @hidden_word[i] == " " then @hidden_word[i] = "*"
+  #     end
+  #   end
+  #   return @hidden_word
+  # end
+
 
   def letter_in_guess(guessed_letter)
-    return @word.include?(guessed_letter)
+    return @hidden_word.include?(guessed_letter)
   end
+
+  # def reveal_guessed_letter(guessed_letter)
+  #   if @word.letter_in_guess(guessed) == true
+  #     @display =
 
 end
--- specs/hidden_word_spec.rb
+++ specs/hidden_word_spec.rb
@@ -5,7 +5,7 @@ class TestHidden_Word < MiniTest::Test
 
   def setup
 
-    @word1 = "Lunch"
+    @word1 = "Lunch time"
     @word2 = "String with spaces"
 
     @hidden_word1 = Hidden_Word.new(@word1)
@@ -18,16 +18,18 @@ class TestHidden_Word < MiniTest::Test
 
   def test_of_hidden_word_creation__no_space
     display_word_given = @hidden_word1.display()
-    word_given = @hidden_word1.word()
+    word_given = @hidden_word1.hidden_word()
     assert_equal(@word1, word_given)
-    assert_equal("*****", display_word_given)
+    assert_equal("***** ****", display_word_given)
   end
 
   def test_letter_in_guess
     assert_equal(true, @hidden_word1.letter_in_guess(@guess1))
   end
 
-  def test_reveal_guessed_letter
-  end 
+  # def test_reveal_guessed_letter
+  #   result = @hidden_word1.display(@guess1)
+  #   assert_equal("*u***", result)
+  # end
 
 end
