2a159cb8c61644250e5e1e0ea2154b9ee2a2b5e1
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-14 18:09:44 +0000

    end of coding session commit. game in progress

--- game.rb
+++ game.rb
@@ -6,6 +6,14 @@ class Game
     @player = input_player
     @game_word = input_game_word
     @guessed_letters = []
-  end 
+  end
+
+  def user_makes_guess(guess_letter)
+    if @game_word.letter_in_guess(guess_letter) == true
+      return @guessed_letters << guess_letter
+    else
+      @player.lives -= 1
+    end
+  end
 
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -18,12 +18,30 @@ class TestGame < MiniTest::Test
 
     @game1 = Game.new(@player1, @hidden_word1)
 
+    @guess1 = "u"
+    @guess2 = "c"
+    @guess3 = "x"
   end
 
   def test_of_game_creation
     assert_equal(@player1, @game1.player())
-    assert_equal(@word1, @game1.game_word.word())
+    assert_equal(@word1, @game1.game_word.hidden_word())
     assert_equal(Array, @game1.guessed_letters.class)
   end
+
+  def test_of_user_makes_guess__in_word
+    @game1.user_makes_guess(@guess1)
+    assert_equal(1, @game1.guessed_letters.length)
+  end
+
+  def test_of_user_makes_guess__not_in_word
+    @game1.user_makes_guess(@guess3)
+    assert_equal(0, @game1.guessed_letters.length)
+  end
+  # def test_of_guessed_letters__not_in_word
+  #   result = @game1.game_word.letter_in_guess(@guess3)
+  #   assert_equal(false, result)
+  #   assert_equal(0, @game1.guessed_letters.length)
+  # end
 
 end
